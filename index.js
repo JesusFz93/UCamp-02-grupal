5790816ee68d8d9074a039e6b873900cc349eaaa
Jesus Fernandez <jesus.fdz93@gmail.com>
2021-11-21 22:25:52 -0600

    Creacion del proyecto

---
+++ index.js
@@ -0,0 +1,52 @@
+// Parte1
+const paises = new Promise((resolve, reject) => {
+    const datos = [
+        { id: 1, nombre: 'México' },
+        { id: 2, nombre: 'Peru' },
+        { id: 3, nombre: 'Colombia' },
+      ];
+
+    resolve(datos);
+});
+
+const estados = new Promise((resolve, reject) => {
+
+    const datos = [
+        { id: 1, nombre: 'Aguascalientes', idPais: 1 },
+        { id: 2, nombre: 'Baja california', idPais: 1 },
+        { id: 3, nombre: 'Baja california sur', idPais: 1 },
+        { id: 4, nombre: 'Campeche', idPais: 1 },
+        { id: 5, nombre: 'Guainía', idPais: 3 },
+        { id: 5, nombre: 'Huila', idPais: 3 },
+  ];
+    
+    resolve(datos);
+});
+
+async function ejecutarPromesas() {
+    try {
+        const getPaises = await paises;
+        const getEstados = await estados;
+
+        const estadosDeMexico = getEstados.filter(estado => estado.idPais === 1);
+        const estadosDeMexicoMayus = estadosDeMexico.map(estado => estado.nombre.toUpperCase());
+        
+        console.log(estadosDeMexicoMayus);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+ejecutarPromesas();
+
+// async function obtenerJson() {
+//     const resp = await fetch('nombres.json');
+//     const data = await resp.json();
+//     console.log(data);
+// }
+
+// obtenerJson() ;
+
+
+// Parte2
